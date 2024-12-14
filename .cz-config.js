cae5acdd3f32a743addf16a840f492634fdd302b
海绵宝宝 <a111@MAC.local>
2024-12-14 16:39:06 +0800

    :rocket: chore(主工程): 工程化配置

--- .cz-config.js
+++ .cz-config.js
@@ -1,3 +1,17 @@
+const readFiles = require('./scripts/readFiles');
+const path = require('path');
+
+const srcPath = path.resolve(process.cwd(), './src');
+
+const scopes = readFiles(srcPath)
+  .map(({ dirName }) => {
+    return {
+      value: dirName,
+      name: dirName,
+    };
+  })
+  .concat({ value: '主工程', name: '主工程' });
+
 module.exports = {
   types: [
     {
@@ -58,6 +72,25 @@ module.exports = {
     footer: '请输入要关闭的issue(可选)',
     confirmCommit: '确定提交此说明吗？',
   },
+  // scope 类型（定义之后，可通过上下键选择）
+  scopes: scopes,
+  //   scopes: [
+  //     ['components', '组件相关'],
+  //     ['hooks', 'hook 相关'],
+  //     ['utils', 'utils 相关'],
+  //     ['element-ui', '对 element-ui 的调整'],
+  //     ['styles', '样式相关'],
+  //     ['deps', '项目依赖'],
+  //     ['auth', '对 auth 修改'],
+  //     ['other', '其他修改'],
+  //     // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+  //     ['custom', '以上都不是？我要自定义'],
+  //   ].map(([value, description]) => {
+  //     return {
+  //       value,
+  //       name: `${value.padEnd(30)} (${description})`,
+  //     };
+  //   }),
 
   // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
   // allowCustomScopes: true,
---
+++ scripts/readFiles.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+/**
+ * 递归遍历，获取指定文件夹下面的所有文件路径
+ */
+function getAllFiles(filePath) {
+  let allFilePaths = [];
+  if (fs.existsSync(filePath)) {
+    const files = fs.readdirSync(filePath);
+    for (let i = 0; i < files.length; i++) {
+      let file = files[i]; // 文件名称（不包含文件路径）
+      let currentFilePath = filePath + '/' + file;
+      let stats = fs.lstatSync(currentFilePath);
+      if (stats.isDirectory()) {
+        allFilePaths.push({
+          dirName: file,
+          path: currentFilePath,
+        });
+      }
+    }
+  } else {
+    console.warn(`指定的目录${filePath}不存在！`);
+  }
+
+  return allFilePaths;
+}
+
+module.exports = function readFiles(srcDir) {
+  let allFiles = getAllFiles(srcDir);
+
+  console.log(`文件数量:${allFiles.length}`);
+  return allFiles;
+};
