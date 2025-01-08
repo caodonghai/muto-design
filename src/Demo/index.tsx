c896f57610919cfeae399d5df20d1632b09ae74d
海绵宝宝 <a111@MAC.local>
2025-01-08 19:11:55 +0800

    feat(Demo): 类型定义

--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -2,16 +2,11 @@ import cls from 'classnames';
 import React, { type FC } from 'react';
 
 import { usePrefix } from '../common';
+import { IDemoProps } from './interface';
 
 import './index.less';
 
-const Demo: FC<{
-  /**
-   * @description 属性描述
-   * @default "默认值"
-   */
-  title?: string;
-}> = (props) => {
+const Demo: FC<IDemoProps> = (props) => {
   const prefixFun = usePrefix('demo');
 
   return (
---
+++ src/Demo/interface.ts
@@ -0,0 +1,7 @@
+export interface IDemoProps {
+  /**
+   * @description 属性描述
+   * @default "默认值"
+   */
+  title?: string;
+}
